ffb5b09a582e2127cf72ae922ed69e94f2f5c068
jxmp <mapic91@163.com>
2015-01-23 10:27:06 +0800

    improve

--- Engine/JxqyGame.cs
+++ Engine/JxqyGame.cs
@@ -333,6 +333,8 @@ namespace Engine
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
+            Content.Dispose();
+            Utils.ClearAsfCache();
         }
 
         /// <summary>
